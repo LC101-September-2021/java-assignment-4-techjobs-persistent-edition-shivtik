71d6d86abfe187af78e6c91a671a0d3f156f547c
Shivani <shivani>
2022-03-21 13:29:58 -0500

    complete part 1 and 2

--- src/main/java/org/launchcode/techjobs/persistent/models/Employer.java
+++ src/main/java/org/launchcode/techjobs/persistent/models/Employer.java
@@ -1,6 +1,22 @@
 package org.launchcode.techjobs.persistent.models;
 
+import javax.persistence.Entity;
+import javax.persistence.OneToOne;
+
+@Entity
 public class Employer extends AbstractEntity {
 
+    public Employer() {
+    }
 
+    @OneToOne
+    private String location;
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
 }
